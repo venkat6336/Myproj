646494727d793c6a7a001a4ba32b606e2a65c46c
Venkyminds <30307136+Venkyminds@users.noreply.github.com>
2019-03-08 18:22:15 +0530

    My FIrst COmmit

---
+++ aem-guides-wknd.ui.apps/src/main/content/jcr_root/apps/wknd/clientlibs/clientlib-site/components/page/page.js
@@ -0,0 +1,21 @@
+(function (element, $) {
+    'use strict';
+    var target = $(element),
+        className = "scrolly",
+        scroll,
+        mobileBreakpoint = 992;
+      
+    if($(window).scrollTop() > 0) {
+        target.addClass(className);
+    }
+      
+    $(window).scroll(function(){
+           
+         scroll = $(window).scrollTop();
+    if(scroll > 0 ) {
+        target.addClass(className);
+    } else {
+        target.removeClass(className);
+    }
+});
+}('body',jQuery));
